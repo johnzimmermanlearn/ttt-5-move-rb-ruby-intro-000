b8027cc92cc097c4bb0ad0828d0f08d006a13f4f
John Zimmerman <john.zimmerman@simplybusiness.com>
2019-03-03 21:09:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,8 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(user_input).to_i
-  puts "#{user_input}".to_i - 1
+def input_to_index(user_input)
+  puts "#{user_input}" - 1
 end
 
 user_input = "1"
